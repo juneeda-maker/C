10cff09f088d873498745573db3fcebb2b0db2c4
juneeda-maker <flora9511@daum.net>
2020-10-31 23:37:33 -0700

    범죄자 조사 프로젝트

--- nanocoding/printfscanf.c
+++ nanocoding/printfscanf.c
@@ -44,10 +44,40 @@ int main(void)
     /*char c = 'A';
     printf("%c\n", c);*/
 
-    char str[256];
+    /*char str[256];
     scanf("%s", str, sizeof(str));
-    printf("%s\n", str);
+    printf("%s\n", str); */
 
+    //프로젝트 
+    //경찰관이 범죄자의 정보를 입수 (조서 작성)
+    //이름? 나이? 몸무게? 키? 범죄명? 
+    char name[256];
+    printf("이름이 뭐에요?");
+    scanf("%s", name, sizeof(name));
+
+    int age;
+    printf("몇살이에요?");
+    scanf("%d", &age);
+
+    float weight;
+    printf("몸무게는 몇 KG 이에요?");
+    scanf("%f", &weight);
+
+    double height;
+    printf("키는 몇 cm 이에요?");
+    scanf("%lf", &height);
+
+    char what[256];
+    printf("무슨 범죄를 저질렀어요?");
+    scanf("%s", what, sizeof(what));
+
+    //조사 내용 출력
+    printf("\n\n------ 범죄자 정보 ------\n\n");
+    printf("이름       : %s\n", name);
+    printf("나이       : %d\n" , age);
+    printf("몸무게      : %.2f\n", weight);
+    printf("키         : %.2lf\n", height);
+    printf("범죄       : %s\n", what);
     return 0;
 
 }
