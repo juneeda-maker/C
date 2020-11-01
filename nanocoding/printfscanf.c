b09268a660f16722ecf93f511b0df4f45be9903e
juneeda-maker <flora9511@daum.net>
2020-10-31 22:38:57 -0700

    상수

--- nanocoding/printfscanf.c
+++ nanocoding/printfscanf.c
@@ -9,10 +9,16 @@ int main(void)
     return 0;*/
 
     //실수형 변수에 대한 예제
-    float f = 46.5f;
+    /*float f = 46.5f;
     printf("%.2f\n", f);
     double d = 4.428;
-    printf("%.2lf\n", d);
+    printf("%.2lf\n", d);*/
+
+    const int YEAR = 2000; //상수
+    printf("태어난 년도 : %d\n", YEAR);
+    
+    
+    
     return 0;
 
 }
