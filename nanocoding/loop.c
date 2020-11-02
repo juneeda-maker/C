d3ac45fa3118a037437132a6f3c769f4abd091d5
juneeda-maker <flora9511@daum.net>
2020-11-01 19:30:26 -0800

    뿔뿔 증감 연산

---
+++ nanocoding/loop.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void) {
+
+    /*printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("Hello World\n");*/
+
+    //++ 뿔뿔
+    /*int a = 10;
+    printf("a는 %d\n", a);
+    a++;
+    printf("a는 %d\n", a); */
+
+   /* int b = 20;
+    //b = b + 1;
+    printf("b는 %d\n", ++b); //++ 먼저 수행 그 결과 b = 21
+    printf("b는 %d\n", b++); // b = 21이니 출력하고 실행 끝난뒤에 ++ 실행 b = 22
+    printf("b는 %d\n", b); //22출력
+    */
+
+   int i = 1;
+   printf("Hello World %d\n", i++); //1
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+   printf("Hello World %d\n", i++);
+
+
+    return 0;
+}
