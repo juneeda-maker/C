2afd58566bc9010778cbfe0be36fb2cdb300b920
juneeda-maker <flora9511@daum.net>
2020-11-02 22:11:17 -0800

    for문

--- nanocoding/loop.c
+++ nanocoding/loop.c
@@ -26,18 +26,24 @@ int main(void) {
     printf("b는 %d\n", b); //22출력
     */
 
-   int i = 1;
-   printf("Hello World %d\n", i++); //1
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
-   printf("Hello World %d\n", i++);
+  // int i = 1;
+  // printf("Hello World %d\n", i++); //1
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
+  // printf("Hello World %d\n", i++);
 
+    //반복문
+    //for, while, do while
 
+    //for(선언; 조건; 증감)
+    for(int i = 1; i <= 10; i++){
+        printf("Hello World %d\n", i);
+    }
     return 0;
 }
