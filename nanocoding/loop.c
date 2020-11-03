716a9eebcfb58e4c9ead3a676e2b4e2b967453d1
juneeda-maker <flora9511@daum.net>
2020-11-02 23:01:29 -0800

    이중 반복문(거꾸로 별)

--- nanocoding/loop.c
+++ nanocoding/loop.c
@@ -95,14 +95,45 @@ int main(void) {
     *****
     */
 
-    for(int i = 0; i < 5; i++)
+    
+    /*for(int i = 0; i < 5; i++)
     {
         for (int j = 0; j <= i; j++)
         {
             printf("*");
         }
         printf("\n");
-    }
+    }*/
 
+    //이중 반복문 (거꾸로 별)
+    /* 
+         *
+       * * 
+     * * *   
+   * * * *
+ * * * * *
+    */
+
+
+   /*
+    SSSS*
+    SSS**
+    SS***
+    S****
+    *****
+   */
+
+    for(int i = 0; i < 5; i++)
+    {   
+       
+        for (int j = i; j < 5; j++)
+        {
+            printf(" ");
+        }
+        for(int k = 0; k <= i; k++){
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
