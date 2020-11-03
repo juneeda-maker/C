4beab87740a442481988bc6e2f6c5c53a1f54a91
juneeda-maker <flora9511@daum.net>
2020-11-02 22:15:36 -0800

    while

--- nanocoding/loop.c
+++ nanocoding/loop.c
@@ -41,9 +41,19 @@ int main(void) {
     //반복문
     //for, while, do while
 
-    //for(선언; 조건; 증감)
-    for(int i = 1; i <= 10; i++){
-        printf("Hello World %d\n", i);
+    //for(선언; 조건; 증감) { }
+   // for(int i = 1; i <= 10; i++){
+   //     printf("Hello World %d\n", i);
+   // }
+   
+   //while (조건) { }
+   int i = 1;
+    while(i <= 10) 
+    {
+        printf("Hello World %d\n", i++);
+        //i++;
     }
+
+
     return 0;
 }
