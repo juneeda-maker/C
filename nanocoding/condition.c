727cba3d7df50671742bea0a37323057ffa3bc03
juneeda-maker <flora9511@daum.net>
2020-12-02 23:52:25 +0900

    switch Case

--- nanocoding/condition.c
+++ nanocoding/condition.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void)
 {
@@ -61,7 +62,7 @@ int main(void)
     }
     
 */
-
+/*
     // && ||
     int a = 10;
     int b = 11;
@@ -73,6 +74,53 @@ int main(void)
     else{
         printf("값이 서로 다르네요\n");
     }
+*/
 
+    //가위 0 바위 1 보 2
+    /*
+    srand(time(NULL));
+    int i = rand() % 3; //0 ~ 2 반환
+    if(i == 0){
+        printf("가위\n");
+    }
+    else if (i == 1){
+        printf("바위\n");
+    }
+    else if (i == 2){
+        printf("보\n");
+    }
+    else{
+        printf("몰라요\n");
+    }
+    */
+   /*
+    srand(time(NULL));
+    int i = rand() % 3; //0 ~ 2 반환
+    switch (i)
+    {
+    case 0:printf("가위\n"); break;
+    case 1:printf("바위\n"); break;
+    case 2:printf("보\n"); break;
+    default:printf("몰라요\n"); break;
+    */
+
+
+   int age = 1;
+    switch (age)
+    {
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:printf("초등학생입니다\n"); break;
+    case 14:
+    case 15:
+    case 16:printf("중학생입니다\n"); break;
+    case 17:
+    case 18:
+    case 19:printf("고등학생입니다\n"); break;
+    default: printf("학생이 아닌가봐요\n");break;
+    }
     return 0;
 }
