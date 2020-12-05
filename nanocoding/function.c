19c8a11df13b4461d77e119aa7699ae4c39a679a
juneeda-maker <flora9511@daum.net>
2020-12-06 00:08:18 +0900

    반환값이 없는 함수

--- nanocoding/function.c
+++ nanocoding/function.c
@@ -2,9 +2,11 @@
 
 //선언
 void p(int num);
+void function_without_return();
 
 int main(void)
 {
+    /*
     //function
     // 계산기
 
@@ -32,6 +34,13 @@ int main(void)
     num /= 6;
     //printf("num은 %d 입니다\n", num); //2
     p(num);
+    */
+
+
+   //함수 종류
+   //반환값이 없는 함수
+    function_without_return();
+
 
     return 0;
 }
@@ -40,4 +49,14 @@ int main(void)
 void p(int num)
 {
     printf("num 은 %d 입니다\n", num);
+}
+
+
+//전달값 : 36
+//함수: num + 4
+//반환형:  int
+//반환형 함수이름(전달값)
+
+void function_without_return(){
+    printf("반환값이 없는 함수입니다\n");
 }
