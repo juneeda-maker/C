c499421a47d956e3e5cfbafd5b573f26296cb6ac
juneeda-maker <flora9511@daum.net>
2020-12-07 01:08:21 +0900

    함수를 이용한 계산기

--- nanocoding/function.c
+++ nanocoding/function.c
@@ -9,6 +9,11 @@ void function_without_params();
 void function_with_params(int num1, int num2, int num3);
 
 int apple(int total, int ate); //전제 total 개에서 ate 개를 먹고 남은 수를 반환하는 함수
+int add(int num1, int num2);
+
+int sub(int num1, int num2);
+int mul(int num1, int num2);
+int div(int num1, int num2);
 
 int main(void)
 {
@@ -61,7 +66,21 @@ int main(void)
     //파라미터(전달값)도 받고, 반환값이 있는 함수
     //int ret = apple(5, 2); // 5개의 사과중에서 2개를 먹었어요 
     //printf("사과 5개중에 2개를 먹으면? %d개가 남아요\n", ret);
-    printf("사과 %d개 중에 %d개를 먹으면 %d개가 남아요.\n", 10, 4 , apple(10,4));
+    //printf("사과 %d개 중에 %d개를 먹으면 %d개가 남아요.\n", 10, 4 , apple(10,4));
+
+    //계산기 함수
+    int num = 2;
+    num = add(num , 3);
+    p(num);
+
+    num = sub(num , 1);
+    p(num);
+
+    num = mul(num, 3);
+    p(num);
+
+    num = div(num, 6);
+    p(num);
 
     return 0;
 }
@@ -100,4 +119,20 @@ void function_with_params(int num1, int num2, int num3){
 int apple(int total, int ate){
     printf("전달값과 반환값이 있는 함수입니다.\n");
     return total - ate;
+}
+
+int add(int num1, int num2){
+    return num1 + num2;
+}
+
+int sub(int num1, int num2){
+    return num1 - num2;
+}
+
+int mul(int num1, int num2){
+    return num1 * num2;
+}
+
+int div(int num1, int num2){
+    return num1/num2;
 }
