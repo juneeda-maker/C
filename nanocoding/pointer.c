3adc5fbc956748ea4535d3bbb6a290ad2841becd
juneeda-maker <flora9511@daum.net>
2020-12-09 17:50:39 +0900

    배열과 포인터의 관계

--- nanocoding/pointer.c
+++ nanocoding/pointer.c
@@ -9,6 +9,7 @@ int main(void){
     //[민수] : 301호
     //문앞에 '암호'가 걸려있음
 
+    /*
     int 철수 = 1; //암호 : 1
     int 영희 = 2;
     int 민수 = 3;
@@ -70,6 +71,49 @@ int main(void){
     //참고로 미션맨이 사는 곳의 주소는 &미션맨 으로 확인
     printf("미션맨의 주소 : %d\n", &미션맨);
     printf("스파이의 주소 : %d\n", &스파이);
+    */
+
+   //배열 ?
+    int arr[3] = {5, 10, 15};
+    int * ptr = arr;
+    for(int i = 0; i<3; i++)
+    {
+        printf("배열 arr[%d] 의 값 : %d\n", i, arr[i]);
+    }
+    for(int i = 0; i<3; i++)
+    {
+        printf("배열 ptr[%d] 의 값 : %d\n", i, ptr[i]);
+    }
+
+    ptr[0] = 100;
+    ptr[1] = 200;
+    ptr[2] = 300;
+
+    for(int i = 0; i<3; i++)
+    {
+        //printf("배열 arr[%d] 의 값 : %d\n", i, arr[i]);
+        printf("배열 arr[%d] 의 값 : %d\n", i, *(arr + i));
+    }
+
+    for(int i = 0; i<3; i++)
+    {
+        //printf("배열 ptr[%d] 의 값 : %d\n", i, ptr[i]);
+        printf("배열 ptr[%d] 의 값 : %d\n", i, *(arr + i));
+    }
+    // *(arr + i) == arr[i] 똑같은 표현
+    // arr == arr 배열의 첫번째 값의 주소와 동일 == &arr[0]
+    printf("arr 자체의 값 : %d\n", arr);
+    printf("arr[0] 의 주소 : %d\n", &arr[0]);
+
+    printf("arr 자체의 값이 가지는 주소의 실제 값 : %d\n", *arr); // *(arr + 0)
+    printf("arr[0] 의 실제 값 : %d\n", *&arr[0]);
+
+
+    // *&는 아무것도 없는것과 같다. & 는 주소이며, * 는 그 주소의 값이기 떄문에,
+    //*& 는 상쇄된다.
+
+    printf("arr[0] 의 실제 값 : %d\n", *&*&*&*&*&*&*&*&*&*&*&arr[0]);
+    printf("arr[0] 의 실제 값 : %d\n", arr[0]);
 
     return 0;
 }
