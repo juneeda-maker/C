a5946077e0288dd4fcdb2887a6f27e18c39efa7a
juneeda-maker <flora9511@daum.net>
2020-12-12 01:18:33 +0900

    구조체 사용

--- nanocoding/struct.c
+++ nanocoding/struct.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-struct Gameinfo {
+struct GameInfo {
     char * name; 
     int year;
     int price;
@@ -28,9 +28,31 @@ int main(void)
 
     char * name2 = "너도게임";
     int year2 = 2017;
-    int price2 = 50;
+    int price2 = 100;
     char * company2 = "너도회사";
 
+    //구조체 사용(struct GameInfo까지가 자료형)
+    struct GameInfo gameInfo1;
+    gameInfo1.name = "나도게임";
+    gameInfo1.year = 2017;
+    gameInfo1.price = 50;
+    gameInfo1.company = "나도회사";
+
+    //구조체 출력
+    printf("-- 게임 출시 정보 --\n");
+    printf("  게임명    : %s\n", gameInfo1.name);
+    printf("  발매년도  : %d\n", gameInfo1.year);
+    printf("  가격      : %d\n", gameInfo1.price);
+    printf("  제작사    : %s\n", gameInfo1.company);
+
+    //구조체 배열처럼 초기화
+    struct GameInfo gameInfo2 = {"너도게임", 2017, 100, "너도회사"};
+
+    printf("-- 또 다른 게임 출시 정보 --\n");
+    printf("  게임명    : %s\n", gameInfo2.name);
+    printf("  발매년도  : %d\n", gameInfo2.year);
+    printf("  가격      : %d\n", gameInfo2.price);
+    printf("  제작사    : %s\n", gameInfo2.company);
 
     return 0;
 }
