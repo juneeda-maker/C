c904e61ebf899145c66c537776ec38c20e617033
juneeda-maker <flora9511@daum.net>
2020-12-12 17:20:35 +0900

    typedef

--- nanocoding/struct.c
+++ nanocoding/struct.c
@@ -9,6 +9,17 @@ struct GameInfo {
     struct GameInfo * friendGame; //연관업체 게임
 };
 
+
+typedef struct  {
+    char * name; 
+    int year;
+    int price;
+    char * company;
+
+    struct GameInfo * friendGame; //연관업체 게임
+} GAME_INFO;
+
+
 int main(void)
 {
     //[게임 출시]
@@ -90,6 +101,27 @@ int main(void)
     printf("  가격      : %d\n", gameInfo1.friendGame->price);
     printf("  제작사    : %s\n", gameInfo1.friendGame->company);
 
+    //typedef
+    // 자료형에 별명을 지정
+    int i = 1;
+    typedef int 정수;
+    typedef float 실수;
+    정수 정수변수 = 3; //int i = 3;
+    실수 실수변수 = 3.23f; //float f = 3.23f;
+    printf("\n\n정수변수 : %d, 실수변수 %.2f\n\n", 정수변수 , 실수변수);
+
+    typedef struct GameInfo 게임정보;
+    게임정보 game1;
+    game1.name = "한글 게임";
+    game1.year = 2015;
+
+
+    GAME_INFO game2;
+    game2.name = "한글 게임2";
+    game2.year = 2014;
+
+    /*struct GameInformation game3;
+    game3.name = "한글게임3";*/
 
 
     return 0;
